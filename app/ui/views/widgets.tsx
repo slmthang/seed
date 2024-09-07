84e020152d7d1002508bfe8f232ea6f0cadac62e
slmthang23 <slmthang23@gmail.com>
2024-09-06 23:58:35 -0400

    working on budget planner page

--- app/ui/views/widgets.tsx
+++ app/ui/views/widgets.tsx
@@ -304,7 +304,7 @@ function RowWidget (
                             <path fillRule="evenodd" d="M20.25 10.332v9.918H21a.75.75 0 0 1 0 1.5H3a.75.75 0 0 1 0-1.5h.75v-9.918a.75.75 0 0 1 .634-.74A49.109 49.109 0 0 1 12 9c2.59 0 5.134.202 7.616.592a.75.75 0 0 1 .634.74Zm-7.5 2.418a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Zm3-.75a.75.75 0 0 1 .75.75v6.75a.75.75 0 0 1-1.5 0v-6.75a.75.75 0 0 1 .75-.75ZM9 12.75a.75.75 0 0 0-1.5 0v6.75a.75.75 0 0 0 1.5 0v-6.75Z" clipRule="evenodd" />
                             <path d="M12 7.875a1.125 1.125 0 1 0 0-2.25 1.125 1.125 0 0 0 0 2.25Z" />
                         </svg>
-                    </div>===
+                    </div>
                     <div className="w-[11rem] h-[90%] rounded-xl flex flex-col justify-center pl-2">
                         <p className="text-base">Netflix Inc.</p>
                         <p className="text-xs font-thin">05/23/2025</p>
