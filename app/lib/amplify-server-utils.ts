6edefe6f935f041c83549d8432294b866b4eccb9
slmthang23 <slmthang23@gmail.com>
2024-09-26 21:06:38 -0400

    clean up;

--- app/lib/amplify-server-utils.ts
+++
@@ -1,9 +0,0 @@
-
-import { authConfig } from "../amplify-cognito-config";
-import { createServerRunner } from '@aws-amplify/adapter-nextjs'
-
-export const { runWithAmplifyServerContext } = createServerRunner ({
-    config: {
-        Auth: authConfig
-    }
-});
